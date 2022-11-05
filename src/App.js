120cd9044d3adf5b7ceea7773bc1259c1f67876f
jasonLapina <lapina.jason@gmail.com>
2022-11-05 17:42:59 +0800

    style fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import About from './components/About/About';
 import Hero from './components/Hero/Hero';
+import Projects from './components/Projects/Projects';
 function App() {
   return (
     <main>
       <Hero />
       <About />
+      <Projects />
     </main>
   );
 }
