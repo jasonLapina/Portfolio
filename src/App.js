8bb5d783976762e1044f37c2d4ab38f44eb725fe
jasonLapina <lapina.jason@gmail.com>
2022-11-06 20:34:57 +0800

    add footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import About from './components/About/About';
+import Footer from './components/Footer/Footer';
 import Hero from './components/Hero/Hero';
 import Projects from './components/Projects/Projects';
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Hero />
       <About />
       <Projects />
+      <Footer />
     </main>
   );
 }
