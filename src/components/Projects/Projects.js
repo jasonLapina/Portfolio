75f9b37298261c4d727b7eab49650c2ffd472ef4
jasonLapina <lapina.jason@gmail.com>
2022-11-06 10:50:24 +0800

    projects stylefix

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -9,16 +9,29 @@ const Projects = () => {
       <h1>My passion projects</h1>
       <div className={classes.projects}>
         <div className={classes.project}>
-          <h3>nomnom</h3>
+          <a href='/'>
+            <h3>nomnom</h3>
+          </a>
+
           <img src={pics.nomnom} alt='Nomnom' />
         </div>
         <div className={classes.project}>
-          <h3>Omnifood</h3>
+          <a href='/'>
+            <h3>Omnifood</h3>
+          </a>
           <img src={pics.omnifood} alt='Omnifood' />
         </div>
         <div className={classes.project}>
-          <h3>Relux</h3>
+          <a href='/'>
+            <h3>Relux</h3>
+          </a>
           <img src={pics.relux} alt='Relux' />
+        </div>
+        <div className={classes.project}>
+          <a href='/'>
+            <h3>pig-game</h3>
+          </a>
+          <img src={pics.piggame} alt='pig-game' />
         </div>
       </div>
     </section>
