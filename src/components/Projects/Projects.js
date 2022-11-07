c7f4170d3efab1c9f2ccb8ddd2a7d4b0ef86eb1b
jasonLapina <lapina.jason@gmail.com>
2022-11-07 13:29:17 +0800

    finish projects section

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,6 +5,16 @@ const Projects = () => {
   const pics = Object.values(ProjectPics);
   const titles = Object.getOwnPropertyNames(ProjectPics);
   const urls = ['https://nomnom-kappa.vercel.app/'];
+  const descriptions = [
+    'Food delivery web app.',
+    'Villa reservation website.',
+    'Food subscription website.',
+    'Local 2 player dice-game.',
+    'Hotel reservation web app.',
+    'To-do web app.',
+    'Number guessing game.',
+    'Real estate website.',
+  ];
 
   return (
     <section>
@@ -14,9 +24,14 @@ const Projects = () => {
           return (
             <div className={classes.project} key={i}>
               <a href={urls[0]} target='_blank' rel='noopener noreferrer'>
-                <h3>{titles[i]}</h3>
+                <h3>
+                  {titles[i]} <ion-icon name='enter-outline'></ion-icon>
+                </h3>
               </a>
-              <img src={pics[i]} alt={titles[i]} />
+              <div className={classes.img}>
+                <img src={pics[i]} alt={titles[i]} />
+              </div>
+              <p className={classes.description}>{descriptions[i]}</p>
             </div>
           );
         })}
