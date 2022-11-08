dbfc0802c87c8b0660fc7132140a2fbb4b59d8ab
jasonLapina <lapina.jason@gmail.com>
2022-11-08 13:49:08 +0800

    fixbug

--- src/components/About/Skills/Logos.js
+++ src/components/About/Skills/Logos.js
@@ -1,14 +1,13 @@
 import classes from './Logos.module.scss';
-import codeLogos from '../../../assets/coding';
-const Logos = () => {
-  const logos = Object.values(codeLogos);
 
-  console.log(logos);
+const Logos = () => {
+  // const logos = Object.values(codeLogos);
+
   return (
     <div className={classes['img-box']}>
-      {logos.map((logo, i) => {
+      {/* {logos.map((logo, i) => {
         return <img key={i} src={logo} alt='coding logo' />;
-      })}
+      })} */}
     </div>
   );
 };
