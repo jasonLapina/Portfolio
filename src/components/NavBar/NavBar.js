e7b6a930a04b51bde7e93a88004f002157c1dfcc
jasonLapina <lapina.jason@gmail.com>
2022-11-12 07:06:32 +0800

    add hero responsiveness

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,19 +4,21 @@ import Navbar from 'react-bootstrap/Navbar';
 import classes from './NavBar.module.scss';
 const NavBar = () => {
   return (
-    <Navbar fixed='top' collapseOnSelect expand='lg' bg='dark' variant='dark'>
-      <Container className={classes.container}>
-        <Navbar.Toggle aria-controls='responsive-navbar-nav' />
-        <Navbar.Collapse id='responsive-navbar-nav'>
-          <Nav className={`me-auto ${classes.nav}`}>
-            <Nav.Link href='#home'>Home</Nav.Link>
-            <Nav.Link href='#about'>About</Nav.Link>
-            <Nav.Link href='#projects'>Projects</Nav.Link>
-            <Nav.Link href='#contacts'>Contacts</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <div className={classes.wrapper}>
+      <Navbar fixed='top' collapseOnSelect expand='lg' bg='dark' variant='dark'>
+        <Container className={classes.container}>
+          <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+          <Navbar.Collapse id='responsive-navbar-nav'>
+            <Nav className={`me-auto ${classes.nav}`}>
+              <Nav.Link href='#home'>Home</Nav.Link>
+              <Nav.Link href='#about'>About</Nav.Link>
+              <Nav.Link href='#projects'>Projects</Nav.Link>
+              <Nav.Link href='#contacts'>Contacts</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
   );
 };
 
